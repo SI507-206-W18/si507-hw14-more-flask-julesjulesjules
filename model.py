0ab2ee2ca0f619b3e973553b953bc756498b63c8
Jules G <juliegil@umich.edu>
2018-04-20 17:30:29 -0400

    added next_id

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
 def init(app):
     global entries
@@ -21,10 +22,11 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
     now = datetime.now()
     time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    entry = {"author": name, "text": text, "timestamp": time_string, "id":str(next_id)}
+    next_id = next_id + 1
     entries.insert(0, entry) ## add to front of list
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
