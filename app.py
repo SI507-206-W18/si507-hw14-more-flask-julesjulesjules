337153caad2b00664ee1e49c9b0e3a58e55a0255
Jules G <juliegil@umich.edu>
2018-04-20 17:36:43 -0400

    added admin route, delete route

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import model
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/admin")
 def index():
     ## print the guestbook
-    return render_template("index.html", entries=model.get_entries())
+    return render_template("admin.html", entries=model.get_entries())
+
 
 @app.route("/add")
 def addentry():
@@ -19,6 +20,12 @@ def postentry():
     message = request.form["message"]
     model.add_entry(name, message)
     return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    id_to_delete = request.form["id"]
+    model.delete_entry(id_to_delete)
+    return redirect("/admin")
 
 if __name__=="__main__":
     model.init()
